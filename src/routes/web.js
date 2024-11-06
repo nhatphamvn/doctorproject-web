2951533f69bdd1f959026bdba3eb721c557f6436
Nhat Pham <turu743@gmail.com>
2024-11-06 14:58:54 +0700

    MVC desigh pattern

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,13 @@
 import express from 'express';
-
+import homeController from '../controller/homeController'
 const router = express.Router()
 
 
+ 
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("hello world");
-    });
+    router.get('/', homeController.handleHelloWorld);
+    router.get('/users', homeController.handleHomePage);
 
-    return app.use("/", router);
+    return app.use("/", router); // định nghĩa đường dẫn đầu tiên
 }
 export default initWebRoutes
